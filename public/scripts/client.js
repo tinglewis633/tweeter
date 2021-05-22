254ee6f4c44682203e06fc3eac765cb90906198e
Lewis <tinglewis633@gmail.com>
2021-05-22 20:22:52 +0000

    use prepend to fix when a user clicks submit a tweet, it does not show up right away.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,6 @@
 //take a user's data as object, and append it with html and css
 const createTweetElement = function (data) {
-  $("#shows").append(` <section class="tweet">
+  $("#shows").prepend(` <section class="tweet">
         <div class="tweet-head">
           <div><img src=${data.user.avatars} /> ${data.user.name}</div>
           <div class="at">${data.user.handle}</div>
@@ -16,8 +16,7 @@ const createTweetElement = function (data) {
           <div>
             <span
               class="need_to_be_rendered"
-              datetime="${data.created_at}"
-            ></span>
+            >${timeago.format(data.created_at)}</span>
             <div class="links">
               <i class="fas fa-flag"></i><i class="fas fa-retweet"></i
               ><i class="fas fa-heart"></i>
@@ -28,6 +27,24 @@ const createTweetElement = function (data) {
 };
 
 $(document).ready(() => {
+  //get array of object using AJAX, and then loop it to call createTweetElement for every object in reverse order
+  const loadtweets = (allTweet) => {
+    const url = `http://localhost:8080/tweets`;
+    $.ajax({ url }).then((response) => {
+      console.log(response);
+      if (allTweet) {
+        for (let index = 0; index <= response.length; index++) {
+          createTweetElement(response[index]);
+        }
+      } else {
+        console.log("hit");
+        console.log("response", response);
+        console.log("last item", response[response.length - 1]);
+        createTweetElement(response[response.length - 1]);
+      }
+    });
+  };
+
   //show compose new tweet after new tweet from top right corner is clicked
   $(".new-tweet").hide();
   $("#create-new-tweet-btn").click(function () {
@@ -53,8 +70,11 @@ $(document).ready(() => {
         method: "POST",
         url: "/tweets",
         data: data,
+      }).then(() => {
+        loadtweets(false);
       });
-      $(".new-tweet").hide();
+      $("#tweet-text").val("");
+      $(".counter").val("140");
     } else {
       $("#alert").empty();
       $("#alert").append(
@@ -62,20 +82,8 @@ $(document).ready(() => {
       );
       return;
     }
-    $("#shows").empty();
-    loadtweets();
+    // $("#shows").empty();
   });
 
-  //get array of object using AJAX, and then loop it to call createTweetElement for every object in reverse order
-  const loadtweets = () => {
-    const url = `http://localhost:8080/tweets`;
-    $.ajax({ url }).then((response) => {
-      for (let index = response.length - 1; index >= 0; index--) {
-        createTweetElement(response[index]);
-      }
-      //use timeago to change time to time ago from now
-      timeago.render(document.querySelectorAll(".need_to_be_rendered"));
-    });
-  };
-  loadtweets();
+  loadtweets(true);
 });
